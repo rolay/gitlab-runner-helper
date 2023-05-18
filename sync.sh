146fa2489decb5d9a9a4ab6bcef25386f551016b
Rolay <rolay8@gmail.com>
2023-05-18 07:09:45 +0000

    修改tag

--- sync.sh
+++ sync.sh
@@ -3,9 +3,10 @@
 set -e
 
 git tag > local.list
-tags=$(glab release list -R gitlab-org/gitlab-runner | tail -n +3 | awk '{ print $1 }' | grep -vf local.list)
+#tags=$(glab release list -R gitlab-org/gitlab-runner | tail -n +3 | awk '{ print $1 }' | grep -vf local.list)
+tags="v14.9.2"
 
-archs="x86_64 arm64 arm s390x ppc64le"
+archs="x86_64 arm64"
 
 for tag in $tags
 do
@@ -16,14 +17,13 @@ do
       arch_target="amd64"
     fi
     skopeo copy --dest-creds="$:$GITHUB_TOKEN" \
-      docker://registry.gitlab.com/gitlab-org/gitlab-runner/gitlab-runner-helper:alpine-latest-$arch-$tag \
-      docker://ghcr.io/loft-orbital/gitlab-runner-helper:alpine-latest-$arch_target-$tag
+      docker://registry.gitlab.com/gitlab-org/gitlab-runner/gitlab-runner-helper:alpine3.15-$arch-$tag \
+      docker://ghcr.io/rolay/gitlab-runner-helper:alpine3.15-$arch_target-$tag
   done
   
   manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm64,linux/arm,linux/s390x,linux/ppc64le \
+    --platforms linux/amd64,linux/arm64 \
     --template ghcr.io/loft-orbital/gitlab-runner-helper:alpine-latest-ARCH-$tag \
-    --target ghcr.io/loft-orbital/gitlab-runner-helper:$tag
+    --target ghcr.io/rolay/gitlab-runner-helper:$tag
   gh release create $tag --generate-notes
 done
-
